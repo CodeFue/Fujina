ba6dfd9ab1b3c8ac3783e5c4f95ce026240d5923
SriCrypt <95457676+SriCrypt@users.noreply.github.com>
2024-03-01 16:37:56 +0530

    Update userRegistration.sol

--- userRegistration.sol
+++ userRegistration.sol
@@ -1,23 +1,27 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity >=0.7.0 <0.9.0;
+contract;
 
-contract userRegistration{
+use libraries::Counter;
 
-    struct Info{
-        string userName;
-        string country;
-        string email;
-        
+storage {
+    /// Internal counter updated via calls from a script.
+    count: u64 = 0,
+}
+
+impl Counter for Contract {
+    #[storage(read, write)]
+    fn increment() -> u64 {
+        let count = storage.count.read() + 1;
+        storage.count.write(count);
+        count
     }
 
-    Info[] public data;
-
-    function registerInstrument(string calldata userName, string calldata country, 
-    string calldata email) external payable {
-        data.push(Info(userName,country,email));
+    #[storage(read)]
+    fn count() -> u64 {
+        storage.count.read()
     }
 
-    function viewRegistration() public view returns (Info[] memory){
-        return data; 
+    #[storage(write)]
+    fn clear() {
+        storage.count.write(0);
     }
-} 
+}
